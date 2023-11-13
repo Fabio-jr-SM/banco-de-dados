4ac22a19c414e0cebe42e0ffef5587faa79acfdc
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-11-13 10:31:20 -0400

    Create exemplo.sql

---
+++ exemplo.sql
@@ -0,0 +1,7 @@
+insert into Customers (customer_id,first_name,last_name,age,country) values (6,"Fulano","Ciclano",20,"BR"); 
+
+delete from Customers where first_name="John" and last_name = "Doe"
+
+select first_name, last_name from Customers where country="USA"
+select count(first_name) from Customers where country="USA"
+
