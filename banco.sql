2398e76836acb975ad8e77396258ddbaddaee29a
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-12-01 10:57:49 -0400

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -12,3 +12,17 @@ on conta.id = transferencia.conta_destino_id
 SELECT conta.numero,transferencia.valor from conta
 join transferencia
 on conta.id = transferencia.conta_origem_id
+
+
+
+SELECT 
+c1.numero as numero_origem, 
+c1.agencia as agencia_origen, 
+c2.numero as numero_desitno, 
+c2.agencia as agencia_destinho,
+transferencia.valor,
+transferencia.data 
+
+from transferencia 
+join conta c1 on transferencia.conta_origem_id = c1.id
+join conta c2 on transferencia.conta_destino_id = c2.id
